9d1cbc04aab772909b8437d282ccf8896347daba
Sheila Aberas <sheila.aberas@gmail.com>
2026-08-19 15:56:39 +0200

    Esta es la estructura del proyecto

---
+++ ejercicios_basicos/ejercicio-01.py
@@ -0,0 +1,6 @@
+# Ejercicio 1
+
+
+# Escribe una función que reciba una cadena de texto como parámetro
+# y devuelva un diccionario con las frecuencias de cada letra en la cadena.
+# Los espacios no deben ser considerados.
